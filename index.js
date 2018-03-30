5b8726df1dc849bebde8ab22570a9f81ffca77e1
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2018-03-30 17:51:00 +0300

    add modules

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 
 var messages = require('./messages.js');
-
-app.use('messages', messages)
+app.use('/messages', messages);
 
 app.listen(3000);
