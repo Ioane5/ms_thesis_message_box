00ed995dab65661fe74cd5eaa9f9bccd0c01776d
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2018-04-01 11:06:52 +0300

    Add Database integration

--- messages.js
+++ messages.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
-router.get('/', function(req, res){
-   res.send('GET route on things.');
+mongoose.connect('mongodb://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASSWORD + '@ds243418.mlab.com:43418/message-box');
+
+var messageSchema = mongoose.Schema({
+    message: String,
+    sharedWith: Array
 });
 
-router.post('/', function(req, res){
-   res.send('POST route on things.');
+var Message = mongoose.model("Message", messageSchema);
+
+router.get('/', function (req, res) {
+    res.send('GET route on things.');
+});
+
+router.post('/', function (req, res) {
+    var messageInfo = req.body;
+    if (!messageInfo.message || !messageInfo.sharedWith || messageInfo.sharedWith.length < 1) {
+        res.status(400);
+        res.send('Invalid Request');
+    } else {
+        var newMessage = new Message({
+            message: messageInfo.message,
+            sharedWith: messageInfo.sharedWith
+        });
+
+        newMessage.save(function (err, Message) {
+            if (err) {
+                console.log(err);
+                res.status(500);
+                res.send('Error, could not save');
+            }
+            else
+                res.send('Message Saved!');
+        });
+    }
 });
 
 //export this router to use in our index.js
