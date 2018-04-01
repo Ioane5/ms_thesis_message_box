bebc655f30b67ca0479b6874022821e75e928677
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2018-04-01 18:55:41 +0300

    allow cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var messages = require('./messages.js');
 
 var app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/messages', messages);
 
